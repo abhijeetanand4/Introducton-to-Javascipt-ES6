f397b4c3dc2bd9776e49f866db96ff5421274730
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-17 18:42:36 +0530

    Create lifeInWeeks.js

---
+++ lifeInWeeks.js
@@ -0,0 +1,17 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var currentAge = 90 - age;
+    var daysLeft = currentAge * 365;
+    var weeksLeft = currentAge * 52;
+    var monthsLeft = currentAge * 12;
+    console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+    
+    
+    
+/*************Don't change the code below**********/
+}
+lifeInWeeks(39);
+
