24965910c9cc489c2057a0fbbfbcad36c56c64af
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-17 18:30:54 +0530

    Create stringslength.js

---
+++ stringslength.js
@@ -0,0 +1,6 @@
+//string length
+
+var char = prompt("Enter post!");
+var charCount = char.length;
+var charLeft = 140 - charCount;
+alert("you have entered " + charCount + " and char left is " + charLeft);
