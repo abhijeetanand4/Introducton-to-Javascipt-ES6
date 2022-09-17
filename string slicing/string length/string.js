dc009e591993c7fa2141535f72925ae7b2984bec
abhijeetanand4 <105948259+abhijeetanand4@users.noreply.github.com>
2022-09-17 18:34:20 +0530

    Delete string slicing/string length directory

--- string slicing/string length/string.js
+++
@@ -1,6 +0,0 @@
-//string slicing
-//string length
-//string casing
-
-var name = prompt("Enter name");
-alert("Hello " + (name.slice(0,1)).toUpperCase() + (name.slice(1,name.length)).toLowerCase());
